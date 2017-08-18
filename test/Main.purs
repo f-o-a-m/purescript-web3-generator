3c0acd1ddf40171dc64fe73544e21cc2ad95e610
James Pak <blinky3713@gmail.com>
2017-08-18 18:52:25 -0400

    using spec for tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,24 +1,35 @@
 module Test.Main where
 
 import Prelude
--- import Test.Spec.Assertions (shouldEqual)
--- import Control.Monad.Eff.Class (liftEff)
+import Test.Spec.Assertions (shouldEqual)
+import Control.Monad.Error.Class (throwError)
 import Control.Monad.Eff (Eff)
--- import Control.Monad.Aff (Aff)
-import Control.Monad.Eff.Exception (EXCEPTION, throw)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (RunnerEffects, run)
+
+import Control.Monad.Eff.Exception (error)
+import Control.Monad.Eff.Console (CONSOLE)
 import Data.Argonaut (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
-import Data.Either (Either, either)
-import Node.Encoding (Encoding(..))
-import Node.FS (FS)
-import Node.FS.Sync (readTextFile)
-import Data.ABI (Abi)
+import Test.Spec (Spec, describe, it)
+import Data.Either (Either, either, isRight)
+import Node.Encoding (Encoding(UTF8))
+import Node.FS.Aff (FS, readTextFile)
+import Data.AbiParser (Abi)
 
-main :: forall e. Eff (console :: CONSOLE, exception :: EXCEPTION, fs :: FS | e) Unit
-main = do
-  ejson <- jsonParser <$> readTextFile UTF8 "./abi-data/SimpleStorage.json"
-  json <- either throw pure ejson
-  logShow $ json
-  let (eabi :: Either String Abi) = decodeJson json
-  logShow $ eabi
+
+
+main :: Eff (RunnerEffects (fs :: FS, console :: CONSOLE )) Unit
+main = run [consoleReporter] $ do
+  simpleStorageParserSpec
+
+simpleStorageParserSpec :: forall r . Spec (fs :: FS, console :: CONSOLE | r) Unit
+simpleStorageParserSpec =
+  describe "simple storage parser spec" do
+
+    it "can parse the simple storage abi" do
+
+       ejson <- jsonParser <$> readTextFile UTF8 "./abi-data/SimpleStorage.json"
+       json <- either (throwError <<< error) pure ejson
+       let (eabi :: Either String Abi) = decodeJson json
+       isRight eabi `shouldEqual` true
