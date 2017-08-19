cb7e421c7c4b4d365d9dd53134edc8dac83034bd
Martin Allen <martin.allen26@gmail.com>
2017-08-19 15:23:18 -0400

    helper functions

--- src/Data/Generator.purs
+++ src/Data/Generator.purs
@@ -1,10 +1,10 @@
 module Data.Generator where
 
 import Prelude
-import Data.String (joinWith, take, drop, toUpper)
-import Data.Array (length)
-import Data.Monoid (mempty)
-import Data.AbiParser (SolidityType(..), SolidityFunction(..), format)
+import Data.String (fromCharArray, joinWith, take, drop, toUpper, toLower)
+import Data.Array (length, replicate, mapWithIndex, unsafeIndex)
+import Partial.Unsafe (unsafePartial)
+import Data.AbiParser (Abi(..), AbiType(..), SolidityType(..), SolidityFunction(..), format)
 
 import Network.Ethereum.Web3.Types (HexString(..), unHex, sha3)
 
@@ -24,6 +24,12 @@ toSelector (SolidityFunction f) =
 capitalize :: String -> String
 capitalize s =
   let h = toUpper $ take 1 s
+      rest = drop 1 s
+  in h <> rest
+
+lowerCase :: String -> String
+lowerCase s =
+  let h = toLower $ take 1 s
       rest = drop 1 s
   in h <> rest
 
@@ -49,37 +55,55 @@ toPSType s = case s of
   SolidityArray a -> "Array " <> toPSType a
 
 --------------------------------------------------------------------------------
--- | Data decleration and instances
+-- | Data decleration, instances, and helpers
 --------------------------------------------------------------------------------
 
+-- | Data declaration
 data DataDecl =
   DataDecl { constructor :: String
-           , argTypes :: Array String
+           , factorTypes :: Array String
            }
-
-instance codeDataDecl :: Code DataDecl where
-  genCode (DataDecl decl) =
-    "data " <> decl.constructor <> " = " <> decl.constructor <> " " <> joinWith " " decl.argTypes
 
 funToDataDecl :: SolidityFunction -> DataDecl
 funToDataDecl (SolidityFunction f) =
   DataDecl { constructor : capitalize f.name
-           , argTypes : map toPSType f.inputs
+           , factorTypes : map toPSType f.inputs
            }
 
+instance codeDataDecl :: Code DataDecl where
+  genCode (DataDecl decl) =
+    "data " <> decl.constructor <> " = " <> decl.constructor <> " " <> joinWith " " decl.factorTypes
+
+-- | Encoding instance
 data BuilderMethod =
   BuilderMethod { unpackExpr :: String
                 , builderExpr :: String
                 }
-
 funToBuilder :: SolidityFunction -> BuilderMethod
 funToBuilder fun@(SolidityFunction f) =
-  let selectorBuilder = "HexString " <> (unHex $ toSelector fun)
-      sep = " <> toDataBuilder "
-      restBuilder = if length f.inputs == 0 then mempty else " <> " <> joinWith sep (map toPSType f.inputs)
-  in BuilderMethod { unpackExpr : capitalize f.name <> " " <> "f"
-                   , builderExpr : selectorBuilder <> restBuilder
+  if length f.inputs == 0 then funToBuilderNoArgs fun else funToBuilderSomeArgs fun
+
+funToBuilderNoArgs :: SolidityFunction -> BuilderMethod
+funToBuilderNoArgs fun@(SolidityFunction f) =
+  let selectorBuilder = "HexString " <> "\"" <> (unHex $ toSelector fun) <> "\""
+  in BuilderMethod { unpackExpr : capitalize f.name
+                   , builderExpr : selectorBuilder
                    }
+
+funToBuilderSomeArgs :: SolidityFunction -> BuilderMethod
+funToBuilderSomeArgs fun@(SolidityFunction f) =
+    let vars = mapWithIndex (\i _ -> "x"<> show i) f.inputs
+        selectorBuilder = "HexString " <> "\"" <> (unHex $ toSelector fun) <> "\""
+        sep = " <> toDataBuilder "
+        restBuilder = if length vars == 1
+                         then " <> toDataBuilder " <> toSingleton (unsafePartial $ unsafeIndex vars 0)
+                         else " <> toDataBuilder " <> toTuple vars
+    in BuilderMethod { unpackExpr : "(" <> capitalize f.name <> " " <> joinWith " " vars <> ")"
+                     , builderExpr : selectorBuilder <> restBuilder
+                     }
+  where
+    toSingleton x = "(Singleton " <> x <> ")"
+    toTuple vars = "(Tuple" <> show (length vars) <> " " <> joinWith ", " vars <> ")"
 
 data AbiEncodingInstance =
   AbiEncodingInstance { instanceType :: String
@@ -103,3 +127,89 @@ instance codeAbiEncodingInstance :: Code AbiEncodingInstance where
         bldr = "\t" <> i.builder
         prsr = "\t" <> i.parser
     in joinWith "\n" [header, bldr, prsr]
+
+--------------------------------------------------------------------------------
+-- | Helper functions (asynchronous call/send)
+--------------------------------------------------------------------------------
+
+callSigPrefix :: Array String
+callSigPrefix = ["Maybe Address", "Address", "CallMode"]
+
+sendSigPrefix :: Array String
+sendSigPrefix = ["Maybe Address", "Address", "BigNumber"]
+
+data HelperFunction =
+  HelperFunction { signature :: Array String
+                 , unpackExpr :: {name :: String, stockArgs :: Array String, payloadArgs :: Array String}
+                 , payload :: String
+                 , transport :: String
+                 }
+
+funToHelperFunction :: SolidityFunction -> HelperFunction
+funToHelperFunction fun@(SolidityFunction f) =
+  let (DataDecl decl) = funToDataDecl fun
+      sigPrefix = if f.constant then callSigPrefix else sendSigPrefix
+      stockVars = mapWithIndex (\i _ -> "x" <> show i) sigPrefix
+      offset = length stockVars
+      conVars = mapWithIndex (\i _ -> "x" <> show (offset + i)) f.inputs
+      helperTransport = toTransportPrefix f.constant $ length f.outputs
+      helperPayload = toPayload decl.constructor conVars
+  in HelperFunction { signature : sigPrefix <> map toPSType f.inputs <> [toReturnType f.constant $ map toPSType f.outputs]
+                    , unpackExpr : {name : f.name, stockArgs : stockVars, payloadArgs : conVars}
+                    , payload : helperPayload
+                    , transport : helperTransport
+                    }
+
+toTransportPrefix :: Boolean -> Int -> String
+toTransportPrefix isCall outputCount =
+  let fun = if isCall then "callAsync" else "sendTxAsync"
+      modifier = if outputCount == 1 then "unSingleton <$> " else ""
+  in modifier <> fun
+
+toPayload :: String -> Array String -> String
+toPayload constr args = case length args of
+  0 -> constr
+  _ -> "(" <> constr <> " " <> joinWith " " args <> ")"
+
+toReturnType :: Boolean -> Array String -> String
+toReturnType constant outputs =
+  if not constant
+     then "Web3MA () HexString"
+     else "Web3MA () " <> case length outputs of
+       0 -> "()"
+       1 -> unsafePartial $ unsafeIndex outputs 0
+       _ -> "(Tuple" <> show (length outputs) <> joinWith " " outputs <> ")"
+
+instance codeHelperFunction :: Code HelperFunction where
+  genCode (HelperFunction h) =
+    let decl = h.unpackExpr.name <> " :: " <> joinWith " -> " h.signature
+        defL = h.unpackExpr.name <> " " <> joinWith " " (h.unpackExpr.stockArgs <> h.unpackExpr.payloadArgs)
+        defR = h.transport <> " " <> joinWith " " h.unpackExpr.stockArgs <> " " <> h.payload
+    in decl <> "\n" <> defL <> " = " <> defR
+
+
+--------------------------------------------------------------------------------
+
+data FunctionCodeBlock = FunctionCodeBlock DataDecl AbiEncodingInstance HelperFunction
+
+funToFunctionCodeBlock :: SolidityFunction -> FunctionCodeBlock
+funToFunctionCodeBlock f = FunctionCodeBlock (funToDataDecl f) (funToEncodingInstance f) (funToHelperFunction f)
+
+instance codeFunctionCodeBlock :: Code FunctionCodeBlock where
+  genCode (FunctionCodeBlock decl@(DataDecl d) inst helper) =
+    let sep = fromCharArray $ replicate 80 '-'
+        comment = "-- | " <> d.constructor
+        header = sep <> " \n " <> comment <> "\n" <> sep
+    in joinWith "\n\n" [ header
+                       , genCode decl
+                       , genCode inst
+                       , genCode helper
+                       ]
+
+instance codeAbi :: Code Abi where
+  genCode (Abi abi) = joinWith "\n\n" <<< map genCode' $ abi
+    where
+      genCode' :: AbiType -> String
+      genCode' at = case at of
+        AbiFunction f -> genCode <<< funToFunctionCodeBlock $ f
+        _ -> ""
--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,6 @@ module Test.Main where
 import Prelude
 import Test.Spec.Assertions (shouldEqual)
 import Control.Monad.Error.Class (throwError)
-import Control.Monad.Eff.Exception.Unsafe (unsafeThrow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -14,14 +13,11 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Argonaut (decodeJson)
 import Data.Argonaut.Parser (jsonParser)
 import Test.Spec (Spec, describe, it)
-import Data.Maybe (Maybe(..))
-import Data.Array (head)
 import Data.Either (Either, either, isRight)
 import Node.Encoding (Encoding(UTF8))
 import Node.FS.Aff (FS, readTextFile)
-import Data.AbiParser (Abi(..), AbiType(..))
-import Data.Generator (genCode, funToEncodingInstance)
-
+import Data.Generator (genCode)
+import Data.AbiParser (Abi)
 
 main :: Eff (RunnerEffects (fs :: FS, console :: CONSOLE )) Unit
 main = run [consoleReporter] $ do
@@ -41,10 +37,5 @@ simpleStorageParserSpec =
     it "can generate an encoding instance" do
        ejson <- jsonParser <$> readTextFile UTF8 "./abi-data/SimpleStorage.json"
        json <- either (throwError <<< error) pure ejson
-       (Abi abi) <- either (throwError <<< error) pure $ decodeJson json
-       let f = head abi
-       case f of
-         Just (AbiFunction fun) -> do
-           _ <- liftEff <<< log <<< genCode <<< funToEncodingInstance $ fun
-           pure unit
-         _ -> unsafeThrow "Didn't get function"
+       (abi :: Abi) <- either (throwError <<< error) pure $ decodeJson json
+       liftEff <<< log <<< genCode $ abi
